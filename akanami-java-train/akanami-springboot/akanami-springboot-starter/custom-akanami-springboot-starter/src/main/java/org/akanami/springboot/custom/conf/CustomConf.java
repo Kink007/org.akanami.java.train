84330968f1ab5cf40f55d63b8bb26c3f5b722a78
super_wyx@163.com <super_wyx@163.com>
2020-06-30 17:38:42 +0800

    提交SpringBoot自定义Starter项目模板

---
+++ akanami-java-train/akanami-springboot/akanami-springboot-starter/custom-akanami-springboot-starter/src/main/java/org/akanami/springboot/custom/conf/CustomConf.java
@@ -0,0 +1,7 @@
+package org.akanami.springboot.custom.conf;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("")
+public class CustomConf {
+}
---
+++ akanami-java-train/akanami-springboot/akanami-springboot-starter/custom-akanami-springboot-starter/src/main/java/org/akanami/springboot/custom/service/CustomService.java
@@ -0,0 +1,4 @@
+package org.akanami.springboot.custom.service;
+
+public interface CustomService {
+}
