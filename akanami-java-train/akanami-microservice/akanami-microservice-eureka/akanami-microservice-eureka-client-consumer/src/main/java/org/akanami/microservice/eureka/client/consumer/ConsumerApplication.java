7a8c331f8086ba36a3718faba301c4b58875adb4
super_wyx@163.com <super_wyx@163.com>
2020-07-02 17:57:16 +0800

    添加Eureka消费者自动负载均衡的例子

--- akanami-java-train/akanami-microservice/akanami-microservice-eureka/akanami-microservice-eureka-client-consumer/src/main/java/org/akanami/microservice/eureka/client/consumer/ConsumerApplication.java
+++ akanami-java-train/akanami-microservice/akanami-microservice-eureka/akanami-microservice-eureka-client-consumer/src/main/java/org/akanami/microservice/eureka/client/consumer/ConsumerApplication.java
@@ -2,6 +2,7 @@ package org.akanami.microservice.eureka.client.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +14,14 @@ public class ConsumerApplication {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
-    @Bean
+    @Bean(value = "nobalance")
     public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean(value = "balance")
+    @LoadBalanced
+    public RestTemplate getBalanceRestTemplate() {
         return new RestTemplate();
     }
 }
--- akanami-java-train/akanami-microservice/akanami-microservice-eureka/akanami-microservice-eureka-client-consumer/src/main/java/org/akanami/microservice/eureka/client/consumer/controller/HelloController.java
+++ akanami-java-train/akanami-microservice/akanami-microservice-eureka/akanami-microservice-eureka-client-consumer/src/main/java/org/akanami/microservice/eureka/client/consumer/controller/HelloController.java
@@ -1,8 +1,10 @@
 package org.akanami.microservice.eureka.client.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +16,13 @@ public class HelloController {
     LoadBalancerClient loadBalancerClient;
 
     @Autowired
+    @Qualifier(value = "nobalance")
     RestTemplate restTemplate;
+
+    @Autowired
+    @Qualifier(value = "balance")
+    RestTemplate autoBalanceRestTemplate;
+
 
     @GetMapping("/hello")
     public String hello() {
@@ -23,5 +31,13 @@ public class HelloController {
         String result = this.restTemplate.getForObject(url, String.class);
 
         return "Consumer Call Provider return:" + result;
+    }
+
+    @GetMapping("/hello1")
+    public String hello1() {
+        //如果此处想直接使用服务名访问，则必须在注入RestTemplate的时候添加上 @LoadBalance注解
+        String result = this.autoBalanceRestTemplate.getForObject("http://AKANAMI-MICROSERVICE-EUREKA-CLIENT-PROVIDER/hello", String.class);
+
+        return "Consumer Call With no balance Provider return:" + result;
     }
 }
