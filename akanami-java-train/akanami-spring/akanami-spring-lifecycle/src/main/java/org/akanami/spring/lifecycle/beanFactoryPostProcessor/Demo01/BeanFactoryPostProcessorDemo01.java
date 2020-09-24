14fd50c3ae902c53375b96fea213b3db624ebdda
akanami <super_wyx@163.com>
2020-09-25 00:38:48 +0800

    添加Spring容器生命周期以及扩展例子

---
+++ akanami-java-train/akanami-spring/akanami-spring-lifecycle/src/main/java/org/akanami/spring/lifecycle/beanFactoryPostProcessor/Demo01/BeanFactoryPostProcessorDemo01.java
@@ -0,0 +1,4 @@
+package org.akanami.spring.lifecycle.beanFactoryPostProcessor.Demo01;
+
+public class BeanFactoryPostProcessorDemo01 {
+}
---
+++ akanami-java-train/akanami-spring/akanami-spring-lifecycle/src/main/java/org/akanami/spring/lifecycle/beanFactoryPostProcessor/Demo01/Mock/AkanamiBeanFactoryPostProcessor.java
@@ -0,0 +1,12 @@
+package org.akanami.spring.lifecycle.beanFactoryPostProcessor.Demo01.Mock;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class AkanamiBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        //configurableListableBeanFactory.
+    }
+}
